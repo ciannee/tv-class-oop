d89ba690a15795f0e66772c6c62d7e1a44b9217b
Patricia Pineda <pepineda28@gmail.com>
2023-05-17 02:21:44 +0800

    yay done!

--- TestTV.py
+++ TestTV.py
@@ -1,4 +1,7 @@
 
+import time
+time.sleep(3)
+
 # import TV from Television file
 from Television import TV
 
@@ -7,6 +10,7 @@ television1 = TV(30, 3, True)
 television2 = TV (3, 2, True)
 
 # print output
-
-television1 = print("tv1's channel is ", television1.channel, "and volume level is ", television1.volume_level)
-television2 = print("tv2's channel is ", television2.channel, "and volume level is ", television2.volume_level)
+print("\n")
+television1 = print("\033[36mtv1's channel is ", television1.channel, "and volume level is ", television1.volume_level)
+television2 = print("\033[95mtv2's channel is ", television2.channel, "and volume level is ", television2.volume_level)
+print ("\n")
