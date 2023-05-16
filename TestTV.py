2974ae232dc32a939ee5c5b23afd2902223c9792
Patricia Pineda <pepineda28@gmail.com>
2023-05-17 02:13:42 +0800

    print output

--- TestTV.py
+++ TestTV.py
@@ -7,3 +7,6 @@ television1 = TV(30, 3, True)
 television2 = TV (3, 2, True)
 
 # print output
+
+television1 = print("tv1's channel is," television1.channel, "and volume level is ", television1.volume_level)
+television2 = print("tv2's channel is," television2.channel, "and volume level is ", television2.volume_level)
