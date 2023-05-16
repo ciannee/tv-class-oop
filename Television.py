9eac6d208f15fe2d598e7c8ec7101dad7a985801
Patricia Pineda <pepineda28@gmail.com>
2023-05-17 02:17:56 +0800

    error sa init hindi indented huhuhu

--- Television.py
+++ Television.py
@@ -8,10 +8,10 @@
 class TV:
     def __init__(self, channel, volume_level, switch):
 
-    # behavior (instance methods)
-    self.channel = channel
-    self.volume_level = volume_level
-    self.switch = switch
+        # behavior (instance methods)
+        self.channel = channel
+        self.volume_level = volume_level
+        self.switch = switch
 
     # turns on this tv
     def turnOn(self):
