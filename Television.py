de06fefc65d106eeb6a25144dda486a541f20bb4
Patricia Pineda <pepineda28@gmail.com>
2023-05-17 01:49:38 +0800

    pseudocode

--- Television.py
+++ Television.py
@@ -2,3 +2,28 @@
 # Section: BSCPE 1-5
 # Assignment 6: UML Class Diagram
 
+# Pseudocode:
+
+# create TV class
+
+    # behavior (instance methods)
+
+    # turns on this tv
+
+    # turns off this tv
+
+    # returns the channel for this tv
+
+    # sets a new channel for this tv
+
+    # gets the volume level for this tv
+
+    # sets a new volume for this tv
+
+    # increases the channel number by 1
+
+    # decreases the channel number by 1
+
+    # increases the volume level by 1
+
+    # decreases the volume level by 1
